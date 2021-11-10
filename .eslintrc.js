c7bd312fe62c344455f3baa5127a473dc462f4a3
caosiwei <1286612323@qq.com>
2021-11-10 10:56:45 +0800

    集成ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:vue/vue3-strongly-recommended', 'standard'],
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'comma-dangle': 0,
+  },
+}
