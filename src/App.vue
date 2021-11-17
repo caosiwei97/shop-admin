8ce186950edb3730e3049b49999473d04c305e45
caosiwei <1286612323@qq.com>
2021-11-17 21:10:43 +0800

    feat: 完成 element 主题定制

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
-  <button @click="$router.push({ path: '/', query: { id: 1 } })">to home</button>
-  <button @click="handleClick">to login</button>
-  <router-view></router-view>
+  <el-button @click="$router.push({ path: '/', query: { id: 1 } })" type="primary"
+    >to home</el-button
+  >
+  <el-button @click="handleClick" size="large">to login</el-button>
+  <el-config-provider :locale="locale">
+    <router-view></router-view>
+  </el-config-provider>
 </template>
 
 <script setup lang="ts">
   import { useRouter } from 'vue-router'
   import { onMounted } from '@vue/runtime-core'
   import { getLoginInfo } from '@/api/common'
+  import zhCn from 'element-plus/lib/locale/lang/zh-cn'
+  import { ElConfigProvider } from 'element-plus'
 
   const router = useRouter()
   const handleClick = () => {
@@ -19,4 +25,6 @@
       console.log(res.data.login_logo)
     })
   })
+
+  const locale = zhCn
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import element from './plugins/element'
 import router from './router'
 import store from './store'
 import './styles/index.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(element).mount('#app')
